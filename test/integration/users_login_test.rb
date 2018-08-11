4f6c5501cd612b0f5b2d590de7d171989c1c0d73
chawes <hawes.conor@gmail.com>
2018-08-11 10:27:46 -0500

    Adding dynamic menu items and logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = User.create(name: "Conor Test", email: "conor@test.com", password: "foobarbaz",
-                      password_confirmation: "foobarbaz")
+    @user = users(:test_user)
   end
 
   test "error flash should not persist across requests" do
@@ -21,4 +20,31 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information and then logout" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: {
+      session: {
+        email: "test@example.com",
+        password: "password"
+      }
+    }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, false
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', logout_path
+    # Logout
+    delete logout_path
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', user_path(@user), false
+    assert_select 'a[href=?]', logout_path, false
+  end
+
 end
